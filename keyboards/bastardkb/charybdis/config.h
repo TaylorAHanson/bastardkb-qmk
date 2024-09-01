5a10540c6b325bb1e2976e4a0ec5814f26f54508
Taylor Hanson <taylor hanson>
2024-08-31 20:47:46 -0400

    flashing my two new keyboards

--- keyboards/bastardkb/charybdis/config.h
+++ keyboards/bastardkb/charybdis/config.h
@@ -31,3 +31,13 @@
 
 // Invert X axis on mouse reports.
 #define POINTING_DEVICE_INVERT_X
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+
+// Enable permissive hold
+#define PERMISSIVE_HOLD
+
